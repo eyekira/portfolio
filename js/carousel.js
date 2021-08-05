60ff33de5800e688fcceadfa8cb91415f8c51db7
eyekira <eyekira@gmail.com>
2021-08-05 16:08:06 -0400

    8.5.21

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,23 @@
-$('.carousel').carousel({
-    interval: 6000,
-    pause: "false"
-  });
+// $('.carousel').carousel({
+//     interval: 6000,
+//     pause: "false"
+//   });
+
+jQuery(document).ready(function(){
+    $('.carousel[data-type="multi"] .item').each(function(){
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<4;i++) {
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+    });
+    });
