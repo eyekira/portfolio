16d8354b5d3340684f1159af3fba604f969360e9
eyekira <eyekira@gmail.com>
2021-08-15 23:42:57 -0400

    scrollspy

--- js/scrollSpy.js
+++ js/scrollSpy.js
@@ -1,3 +1,3 @@
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbar-example'
-  })
+// var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//     target: '#navbar-example'
+//   })
