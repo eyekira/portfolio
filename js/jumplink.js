c48d70513375894f1c7e1bf660791da3b6424193
eyekira <eyekira@gmail.com>
2021-08-16 01:39:16 -0400

    style update on scrollspy

--- js/jumplink.js
+++ js/jumplink.js
@@ -1,37 +1,37 @@
 
-  var hash = window.location.hash
+  // var hash = window.location.hash
 
-  window.location.hash = "";
-  //jQuery(document).ready(function( $ ){
-  jQuery(window).load(function() {
+  // window.location.hash = "";
+  // //jQuery(document).ready(function( $ ){
+  // jQuery(window).load(function() {
     
-    if(hash) {    
-      setTimeout(function(){ scrolltoHash(hash); }, 750);    
-    }
+  //   if(hash) {    
+  //     setTimeout(function(){ scrolltoHash(hash); }, 750);    
+  //   }
     
-    jQuery('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) 
-      {
-            if(this.hash) {
-               scrolltoHash(this.hash);
-          }
-      }
-    });
+  //   jQuery('a[href*=#]:not([href=#])').click(function() {
+  //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) 
+  //     {
+  //           if(this.hash) {
+  //              scrolltoHash(this.hash);
+  //         }
+  //     }
+  //   });
     
-    function scrolltoHash(hash) {
+  //   function scrolltoHash(hash) {
   
-      var headerHeight = jQuery(".masthead").height(); 
-      var hashtop = jQuery(hash).offset().top;
+  //     var headerHeight = jQuery(".header").height(); 
+  //     var hashtop = jQuery(hash).offset().top;
       
-      console.log(hash);
-      console.log(headerHeight);
-      console.log(hashtop);
-      jQuery('html,body').animate({
-         scrollTop: jQuery(hash).offset().top - headerHeight
-       }, 500);
+  //     console.log(hash);
+  //     console.log(headerHeight);
+  //     console.log(hashtop);
+  //     jQuery('html,body').animate({
+  //        scrollTop: jQuery(hash).offset().top - headerHeight
+  //      }, 500);
       
-       return false;    
-    }
+  //      return false;    
+  //   }
   
-  });
+  // });
   
